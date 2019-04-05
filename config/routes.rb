0ce6c700544e29af6fb27b37d57180fa9514eaa8
Takuya <dyhpaax@gmail.com>
2019-04-05 12:21:45 +0900

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/index', to: 'staticpages#index'
   get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   resources :users
 end
