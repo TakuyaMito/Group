f02bfa19472ff37fe42ec53234241d150143e792
Takuya <dyhpaax@gmail.com>
2019-04-06 15:13:12 +0900

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase}
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,12 +8,41 @@ class User < ApplicationRecord
             uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum:6 }
+end
 
+
+  class << self
   #渡された文字列のハッシュ値を返す
-  def User.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                               BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
+  #ランダムなトークを返す
+  def new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #永続セッションのためにユーザーをデータベースに返す
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #渡されtらトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
